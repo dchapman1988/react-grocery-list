7bbb62ef54a1d23234d0394aec229c5025cc1ca3
David Chapman <dchapman1988@gmail.com>
2022-12-08 21:17:36 -0600

    UX for basic grocery list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+// Just a simple grocery list app
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // Use the useState hook to store the list of grocery items
+  const [groceryList, setGroceryList] = useState([]);
+
+  // Function to add a new item to the list
+  const addItem = (item) => {
+    setGroceryList([...groceryList, item]);
+  }
+
+  // Function to remove an item from the list
+  const removeItem = (item) => {
+    setGroceryList(groceryList.filter(i => i !== item));
+  }
+
+	return(
+		<div>
+		<h1>Grocery List</h1>
+		<ul>
+		{groceryList.map(item => (
+			<li key={item}>
+			{item}
+			<button onClick={() => removeItem(item)}>Remove</button>
+			</li>
+		))}
+		</ul>
+		<input
+		type="text"
+		placeholder="Add an item"
+		onKeyDown={(e) => {
+			if (e.key === 'Enter') {
+				addItem(e.target.value);
+				e.target.value = '';
+			}
+		}}
+		/>
+		</div>
   );
 }
 
